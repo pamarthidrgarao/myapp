7900f9e2091ee532cf044f2935a5f0499f42ef06
Pamarthi Durga Rao <durga.pamarthi@broadridge.com>
2017-06-09 16:21:50 +0530

    init

---
+++ app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([])
+    ],
+    declarations: [AppComponent],
+
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
